c0eef049d14a70515fa77bd3e14a55dc693d62e4
Ryan <rpa@ldeo.columbia.edu>
2014-07-13 14:36:49 -0400

    diagnostics updates

--- run_the_model.py
+++ run_the_model.py
@@ -1,13 +1,14 @@
 from pylab import *
 import twolayer_qg
 
-m = twolayer_qg.QGModel(tavestart=0)
+m = twolayer_qg.QGModel(dt=8000, tavestart=0)
 
 figure()
 for snapshot in m.run_with_snapshots(tsnapint=4320000):
     # do something like plot
+    print 'Mean EKE1: %g' % (m.get_diagnostic('EKE1'))
     clf()
-    pcolormesh(m.q1 + m.beta1*m.y)
+    contourf(m.q1 + m.beta1*m.y,18)
     colorbar()
     pause(0.01)
     show()

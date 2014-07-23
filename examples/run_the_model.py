1ddbe298b8146dc0ed7785f8105e5ba4a2fc19af
MFJansen <malte@malte-f-jansens-computer-2.local>
2014-07-22 23:14:26 -0400

    slight fix of run_the_model script

--- examples/run_the_model.py
+++ examples/run_the_model.py
@@ -4,7 +4,7 @@ from pyqg import qg_model
 m = qg_model.QGModel(tavestart=0,  dt=8000)
 
 for snapshot in m.run_with_snapshots(
-        tsnapstart=155520000, tsnapint=m.dt):
+        tsnapstart=155520000, tsnapint=1000*m.dt):
     clf()
     imshow(m.q1)
     show()

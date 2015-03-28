26f9111dfef89c3cdd1e8121e9ba226254d336b3
Cesar B Rocha <crocha@ucsd.edu>
2015-03-27 22:35:44 -0700

    added fft options into QGModel class; added ke print out; fixed annoying DivisionByZero warning

--- pyqg/qg_model.py
+++ pyqg/qg_model.py
@@ -1,11 +1,16 @@
 import numpy as np
-try:
+from numpy import pi
+try:   
     import mkl
     np.use_fastnumpy = True
 except ImportError:
     pass
 
-pi = np.pi
+try:
+    import pyfftw
+    pyfftw.interfaces.cache.enable() 
+except ImportError:
+    pass
 
 class QGModel(object):
     """A class that represents the two-layer QG model."""
@@ -13,9 +18,9 @@ class QGModel(object):
     def __init__(
         self,
         # grid size parameters
-        nx=64,                      # grid resolution
+        nx=64,                     # grid resolution
         ny=None,
-        L=1e6,                      # domain size 
+        L=1e6,                     # domain size is L [m]
         W=None,
         # physical parameters
         beta=1.5e-11,               # gradient of coriolis parameter
@@ -25,7 +30,7 @@ class QGModel(object):
         U1=0.025,                   # upper layer flow
         U2=0.0,                     # lower layer flow
         # timestepping parameters
-        dt=7200.,                   # numerical timstep
+        dt=7200.,                   # numerical timestep
         tplot=10000.,               # interval for plots (in timesteps)
         tcfl=1000.,                 # interval for cfl writeout (in timesteps)
         tmax=1576800000.,           # total time of integration
@@ -33,7 +38,10 @@ class QGModel(object):
         taveint=86400.,             # time interval used for summation in longterm average in seconds
         tpickup=31536000.,          # time interval to write out pickup fields ("experimental")
         # diagnostics parameters
-        diagnostics_list='all'      # which diagnostics to output
+        diagnostics_list='all',     # which diagnostics to output
+        # fft parameter
+        fftw = False,               # fftw flag 
+        ntd = 3,                    # number of threads to use in fftw computations
         ):
         """Initialize the two-layer QG model.
         
@@ -46,7 +54,7 @@ class QGModel(object):
         L -- domain length in x direction, units meters 
         W -- domain width in y direction, units meters (default: L)
         (WARNING: some parts of the model or diagnostics might
-        actually assume nx=ny -- check before making different choice!)
+        actuallye assume nx=ny -- check before making different choice!)
         
         Physical Paremeter Keyword Arguments:
         beta -- gradient of coriolis parameter, units m^-1 s^-1
@@ -72,9 +80,10 @@ class QGModel(object):
         tpickup -- time interval for writing pickup files, units seconds
         (NOTE: all time intervals will be rounded to nearest dt interval)
         """
+
         if ny is None: ny = nx
         if W is None: W = L
-        
+       
         # put all the parameters into the object
         # grid
         self.nx = nx
@@ -96,8 +105,11 @@ class QGModel(object):
         self.tavestart = tavestart
         self.taveint = taveint
         self.tpickup = tpickup
-        
-        # comput timestep stuff
+        # fft 
+        self.fftw = fftw
+        self.ntd = ntd
+
+        # compute timestep stuff
         self.taveints = np.ceil(taveint/dt)      
 
         self.x,self.y = np.meshgrid(
@@ -109,7 +121,7 @@ class QGModel(object):
             1e-7*np.random.rand(self.ny,self.nx) + 1e-6*(
                                 np.ones((self.ny,1)) * np.random.rand(1,self.nx) ),
             np.zeros_like(self.x)        
-        )    
+        )   
 
         # Background zonal flow (m/s):
         self.U = self.U1 - self.U2
@@ -117,11 +129,14 @@ class QGModel(object):
         # Notice: at xi=1 U=beta*rd^2 = c for xi>1 => U>c
 
         # wavenumber one
-        self.k0x = 2*pi/self.L
-        self.k0y = 2*pi/self.W
+        self.k0x = 2.*pi/self.L
+        self.k0y = 2.*pi/self.W
         # wavenumber grids
-        self.kk = np.fft.fftfreq(self.nx, self.L/self.nx)*2*pi
-        self.ll = np.fft.fftfreq(self.ny, self.W/self.ny)*2*pi
+        
+        self.ll = (2.*pi/self.W)*np.append( np.arange(0.,self.nx/2), \
+            np.arange(-self.nx/2,0.) )
+        self.kk = (2.*pi/self.L)*np.arange(0.,self.nx/2+1)
+
         self.k, self.l = np.meshgrid(self.kk, self.ll)
         # physical grid spacing
         self.dx = self.L / self.nx
@@ -139,22 +154,17 @@ class QGModel(object):
         self.del2 = (self.delta+1.)**-1
 
         # isotropic wavenumber^2 grid
-        self.wv2 = (self.k**2 + self.l**2)
+        self.wv2 = np.ma.masked_equal(self.k**2 + self.l**2, 0.).filled(1.e20)
         self.wv2i = self.wv2**-2
-        self.wv2i[0,0] = 0.
 
-        # determine inversion matrix: psi=Aq (i.e. A=B**(-1) where q=B*psi)
-        # this gives a warning because wavenumber 0 entries cause div by 0
-        # they are set to zero later though (could probably write this more elegantly)
-        det = np.ma.masked_equal(self.wv2 * (self.wv2 + self.F1 + self.F2),0.)
-        self.a11 = -((self.wv2 + self.F2)/det).filled(0.)
-        self.a12 = -(self.F1/det).filled(0.)
-        self.a21 = -(self.F2/det).filled(0.)
-        self.a22 = -((self.wv2 + self.F1)/det).filled(0.)
-        #self.a11[0,0] = 0.
-        #self.a12[0,0] = 0.
-        #self.a21[0,0] = 0.
-        #self.a22[0,0] = 0.
+        # determine inversion matrix: psi = A q (i.e. A=M_2**(-1) where q=M_2*psi)
+        det = self.wv2 * (self.wv2 + self.F1 + self.F2)
+        self.a11 = -((self.wv2 + self.F2)/det)
+        self.a12 = -((self.F1)/det)
+        self.a21 = -((self.F2)/det)
+        self.a22 = -((self.wv2 + self.F1)/det)
+
+        self.det = det
 
         # this defines the spectral filter (following Arbic and Flierl, 2003)
         cphi=0.65*pi 
@@ -167,7 +177,7 @@ class QGModel(object):
         # initialize timestep
         self.t=0        # actual time
         self.tc=0       # timestep number
-
+        
         # Set time-stepping parameters for very first timestep (Euler-forward stepping).
         # Adams Bashford used thereafter and is set up at the end of the first time-step (see below)
         self.dqh1dt_p = 0.
@@ -181,26 +191,42 @@ class QGModel(object):
         elif diagnostics_list == 'none':
             self.set_active_diagnostics([])
         else:
-            self.set_active_diagnostics(diagnostics_list)
+            self.set_active_diagnostics(diagnostics_list)   
+
+    def _fft2(self, A):
+        if self.fftw:
+            a = pyfftw.n_byte_align_empty(A.shape, 8, 'float64')
+            a[:]= A.copy()
+            return pyfftw.builders.rfft2(a,threads=self.ntd)()
+        else:
+            return np.fft.rfft2(A)
+
+    def _ifft2(self, Ahat):
+        if self.fftw:
+            ahat = pyfftw.n_byte_align_empty(Ahat.shape, 16, 'complex128')
+            ahat[:]= Ahat.copy()
+            return pyfftw.builders.irfft2(ahat,threads=self.ntd)()
+        else:
+            return np.fft.irfft2(Ahat)
 
     def set_q1q2(self, q1, q2):
         self.q1 = q1
         self.q2 = q2
+
         # initialize spectral PV
-        self.qh1 = np.fft.fft2(self.q1)
-        self.qh2 = np.fft.fft2(self.q2)
-        
+        self.qh1 = self._fft2(self.q1)
+        self.qh2 = self._fft2(self.q2) 
 
     # compute advection in grid space (returns qdot in fourier space)
     def advect(self, q, u, v):
-        return 1j*self.k*np.fft.fft2(u*q) + 1j*self.l*np.fft.fft2(v*q)
+        return 1j*self.k*self._fft2(u*q) + 1j*self.l*self._fft2(v*q)
         
     # compute grid space u and v from fourier streafunctions
     def caluv(self, ph):
-        u = -np.real(np.fft.ifft2(1j*self.l*ph))
-        v = np.real(np.fft.ifft2(1j*self.k*ph))
+        u = -self._ifft2(1j*self.l*ph)
+        v = self._ifft2(1j*self.k*ph)
         return u, v
-    
+  
     # Invert PV for streamfunction
     def invph(self, zh1, zh2):
         ph1 = self.a11*zh1 + self.a12*zh2
@@ -208,7 +234,7 @@ class QGModel(object):
         return ph1, ph2
     
     def run_with_snapshots(self, tsnapstart=0., tsnapint=432000.):
-        """Run the model forward until the next snapshot, then yeild."""
+        """Run the model forward until the next snapshot, then yield."""
         
         tsnapints = np.ceil(tsnapint/self.dt)
         nt = np.ceil(np.floor((self.tmax-tsnapstart)/self.dt+1)/tsnapints)
@@ -221,28 +247,33 @@ class QGModel(object):
                 
     def run(self):
         """Run the model forward without stopping until the end."""
-        while(self.t < self.tmax):
+        while(self.t < self.tmax): 
             self._step_forward()
-    
+
+            if np.isnan(self.calc_ke()):
+                print " *** Blow up  "
+                break
+
     def _step_forward(self):
         # compute grid space qgpv
-        self.q1 = np.real(np.fft.ifft2(self.qh1))
-        self.q2 = np.real(np.fft.ifft2(self.qh2))
+        self.q1 = self._ifft2(self.qh1)
+        self.q2 = self._ifft2(self.qh2)
 
         # invert qgpv to find streamfunction and velocity
         self.ph1, self.ph2 = self.invph(self.qh1, self.qh2)
         self.u1, self.v1 = self.caluv(self.ph1)
         self.u2, self.v2 = self.caluv(self.ph2)
         
-        # check cfl
-        if (self.tc % self.tcfl)==0:
-            print 't=%16d, tc=%10d: cfl=%5.6f' % (
-                   self.t, self.tc, self.calc_cfl())
-        
         # here is where we calculate diagnostics
-        if (self.t>=self.tavestart) and (self.tc%self.taveints==0):
+        if (self.t>=self.dt) and (self.tc%self.taveints==0):
             self._increment_diagnostics()
 
+        # check cfl
+        if (self.tc % self.tcfl)==0:
+            print 't=%16d, tc=%10d: cfl=%5.6f, ke=%5.6f' % (
+                   self.t, self.tc, self.calc_cfl(), \
+                           self.calc_ke() )
+        
         # compute tendency from advection and bottom drag:  
         self.dqh1dt = (-self.advect(self.q1, self.u1 + self.U1, self.v1) 
                   -self.beta1*1j*self.k*self.ph1)
@@ -268,11 +299,14 @@ class QGModel(object):
         # augment timestep
         self.tc += 1
         self.t += self.dt
-
-        
+    
     def calc_cfl(self):
         return np.abs(np.hstack([self.u1 + self.U1, self.v1,
                           self.u2 + self.U2, self.v2])).max()*self.dt/self.dx
+ 
+    def calc_ke(self):
+        return  ( 2.*0.5*self.wv2*( np.abs(self.ph1)**2 + \
+                np.abs(self.ph2)**2 )/ ( (self.nx*self.ny)**2) ).sum()
 
     ### All the diagnostic stuff follows. ###
 
@@ -286,71 +320,73 @@ class QGModel(object):
 
         self.add_diagnostic('entspec',
             description='barotropic enstrophy spectrum',
-            function=(lambda self:
+            function= (lambda self:
                       np.abs(self.del1*self.qh1 + self.del2*self.qh2)**2.)
         )
             
         self.add_diagnostic('APEflux',
             description='spectral flux of available potential energy',
-            function=(lambda self:
+            function= (lambda self:
               self.rd**-2 * self.del1*self.del2 *
               np.real((self.ph1-self.ph2)*np.conj(self.Jptpc)) )
         )
         
         self.add_diagnostic('KEflux',
             description='spectral flux of kinetic energy',
-            function=(lambda self:
+            function= (lambda self:
               np.real(self.del1*self.ph1*np.conj(self.Jp1xi1)) + 
               np.real(self.del2*self.ph2*np.conj(self.Jp2xi2)) )
         )
 
         self.add_diagnostic('KE1spec',
             description='upper layer kinetic energy spectrum',
-            function=(lambda self: 0.5*self.wv2*np.abs(self.ph1)**2)
+            function= (lambda self: 2.*0.5*self.wv2*np.abs(self.ph1)**2/\
+                    ( (self.nx*self.ny)**2) )
         )
         
         self.add_diagnostic('KE2spec',
             description='lower layer kinetic energy spectrum',
-            function=(lambda self: 0.5*self.wv2*np.abs(self.ph2)**2)
+            function= (lambda self: 2.*0.5*self.wv2*np.abs(self.ph2)**2/\
+                    ( (self.nx*self.ny)**2) )
         )
         
         self.add_diagnostic('q1',
             description='upper layer QGPV',
-            function=(lambda self: self.q1)
+            function= (lambda self: self.q1)
         )
 
         self.add_diagnostic('q2',
             description='lower layer QGPV',
-            function=(lambda self: self.q2)
+            function= (lambda self: self.q2)
         )
 
         self.add_diagnostic('EKE1',
             description='mean upper layer eddy kinetic energy',
-            function=(lambda self: 0.5*(self.v1**2 + self.u1**2).mean())
+            function= (lambda self: 0.5*(self.v1**2 + self.u1**2).mean())
         )
 
         self.add_diagnostic('EKE2',
             description='mean lower layer eddy kinetic energy',
-            function=(lambda self: 0.5*(self.v2**2 + self.u2**2).mean())
+            function= (lambda self: 0.5*(self.v2**2 + self.u2**2).mean())
         )
         
         self.add_diagnostic('EKEdiss',
             description='total energy dissipation by bottom drag',
-            function=( lambda self:
+            function= (lambda self:
                        (self.del2*self.rek*self.wv2*
                         np.abs(self.ph2)**2./(self.nx*self.ny)).sum())
         )
         
         self.add_diagnostic('APEgenspec',
             description='spectrum of APE generation',
-            function=( lambda self: self.U * self.rd**-2 * self.del1 * self.del2 *
+            function= (lambda self: self.U * self.rd**-2 * self.del1 * self.del2 *
                        np.real(1j*self.k*(self.del1*self.ph1 + self.del2*self.ph2) *
                                   np.conj(self.ph1 - self.ph2)) )
         )
         
         self.add_diagnostic('APEgen',
             description='total APE generation',
-            function=( lambda self: self.U * self.rd**-2 * self.del1 * self.del2 *
+            function= (lambda self: self.U * self.rd**-2 * self.del1 * self.del2 *
                        np.real(1j*self.k*
                            (self.del1*self.ph1 + self.del2*self.ph2) *
                             np.conj(self.ph1 - self.ph2)).sum() / 
@@ -376,10 +412,10 @@ class QGModel(object):
            
     def _increment_diagnostics(self):
         # compute intermediate quantities needed for some diagnostics
-        self.p1 = np.real(np.fft.ifft2(self.ph1))
-        self.p2 = np.real(np.fft.ifft2(self.ph2))
-        self.xi1 = np.real(np.fft.ifft2(-self.wv2*self.ph1))
-        self.xi2 = np.real(np.fft.ifft2(-self.wv2*self.ph2))
+        self.p1 = self._ifft2(self.ph1)
+        self.p2 = self._ifft2(self.ph2)
+        self.xi1 = self._ifft2(-self.wv2*self.ph1)
+        self.xi2 = self._ifft2(-self.wv2*self.ph2)
         self.Jptpc = -self.advect(
                     (self.p1 - self.p2),
                     (self.del1*self.u1 + self.del2*self.u2),
@@ -400,7 +436,3 @@ class QGModel(object):
     def get_diagnostic(self, dname):
         return (self.diagnostics[dname]['value'] / 
                 self.diagnostics[dname]['count'])
-             
-     
-                  
-               

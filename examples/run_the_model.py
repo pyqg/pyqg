86b5f3cf2bfb52e0e44c7fbf6134a4afc17cf9c7
Ryan Abernathey <rpa@ldeo.columbia.edu>
2015-05-06 14:04:53 -0400

    little tiny change

--- examples/run_the_model.py
+++ examples/run_the_model.py
@@ -6,9 +6,9 @@ m = qg_model.QGModel(tavestart=0,  dt=8000)
 for snapshot in m.run_with_snapshots(
         tsnapstart=0, tsnapint=1000*m.dt):
     clf()
-    imshow(m.q[0])# + m.beta1*m.y)
-    #clim([0,m.beta1*m.L])
-    clim([-m.beta1*m.L/20., m.beta1*m.L/20.])
+    imshow(m.q[0] + m.beta1*m.y)
+    clim([0,m.beta1*m.L])
+    #clim([-m.beta1*m.L/20., m.beta1*m.L/20.])
     show()
     pause(0.01)
 # now the model is done

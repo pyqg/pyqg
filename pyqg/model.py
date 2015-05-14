a4587e3a62a88a14c03b69625ef9fc0214e46729
Ryan Abernathey <rpa@ldeo.columbia.edu>
2015-05-14 17:23:42 -0400

    Revert "added files and started modifying; not a working commit"
    This reverts commit 75c7464557d93173381c55ea4194f130fc7c31b7.

--- pyqg/model.py
+++ pyqg/model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import spectral_kernel
 from numpy import pi
 try:   
     import mkl
@@ -13,13 +12,7 @@ try:
 except ImportError:
     pass
 
-
-# shape and datatype of data
-dtype_real = np.dtype('float64')            
-dtype_cplx = np.dtype('complex128')
-
-
-class Model(spectral_kernel.PseudoSpectralKernel):
+class Model(object):
     """A class that represents a generic pseudo-spectral inversion model."""
     
     def __init__(
@@ -104,12 +97,11 @@ class Model(spectral_kernel.PseudoSpectralKernel):
         self.ntd = ntd
 
         self._initialize_grid()
-        #self._initialize_fft()
+        self._initialize_fft()
         self._initialize_background()
         self._initialize_forcing()
         self._initialize_inversion_matrix()
-        #self._initialize_state_variables()
-        self._initialize_kernel()
+        self._initialize_state_variables()
         self._initialize_time()                
        
         self._initialize_diagnostics()
@@ -206,8 +198,8 @@ class Model(spectral_kernel.PseudoSpectralKernel):
         self.wv2i[iwv2] = self.wv2[iwv2]**-1
         
     def _initialize_background(self):        
-        self.Ubg = np.zeros(self.Nz, dtype_real)
-        self.Qy = np.zeros(self.Nz, dtype_real)
+        raise NotImplementedError(
+            'needs to be implemented by Model subclass')
         
     def _initialize_inversion_matrix(self):
         raise NotImplementedError(
@@ -217,16 +209,12 @@ class Model(spectral_kernel.PseudoSpectralKernel):
         raise NotImplementedError(
             'needs to be implemented by Model subclass')
             
-    def _initialize_kernel(self):
-        super(BTModel, self).__init__(
-            self.nz, self.ny, self.nx,
-            self.a, self.kk, self.ll,
-            self.Ubg, self.Qy
-        )
-            
     def _initialize_state_variables(self):
         """Set up model basic state variables.
         This is universal. All models should store their state this way."""
+        # shape and datatype of data
+        dtype_real = np.dtype('float64')            
+        dtype_cplx = np.dtype('complex128')
         if self.nz > 1:
             shape_real = (self.nz, self.ny, self.nx)
             shape_cplx = (self.nz, self.nl, self.nk)

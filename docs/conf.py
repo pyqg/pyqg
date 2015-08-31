4742ba335f5a6e0935532e409a7992f3e0ede8a0
Ryan Abernathey <rpa@ldeo.columbia.edu>
2015-08-31 12:58:53 -0400

    even more

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['pyfftw', 'scipy']
+MOCK_MODULES = ['pyfftw', 'scipy.interpolate', 'scipy.ndimage']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 

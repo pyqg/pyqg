f04e0cac49632fc5f24bf8d7cf6c920697341dde
Malte F Jansen <mfj@uchicago.edu>
2015-11-03 20:41:51 -0600

    un-broke the barotropic model
    my previous commit broke the barotropic model, which doesn't have a variable Hi
    so as a somewhat lame hack it now has one..

--- pyqg/bt_model.py
+++ pyqg/bt_model.py
@@ -40,6 +40,7 @@ class BTModel(model.Model):
         self.beta = beta
         self.rd = rd
         self.H = H
+        self.Hi = np.array([H]) # this is needed so that the diags in model.py don't break
         self.U = U
         
         self.nz = 1

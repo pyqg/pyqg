829372e815c672cd56cf018a58b5d7931d6b6de5
hscannell <scannell@ldeo.columbia.edu>
2021-06-04 06:42:29 -0400

    run with snapshots

--- pyqg/tests/test_xarray_output.py
+++ pyqg/tests/test_xarray_output.py
@@ -4,27 +4,85 @@ import pyqg
 import xarray as xr
 
 def test_xarray():
-    # Set parameters & initialize model
-    year = 24*60*60*360.
-    twrite = 1000
-    tavestart = 2*year
+    expected_vars = [
+        'q', 
+        'u', 
+        'v', 
+        'ufull', 
+        'vfull', 
+        'qh', 
+        'uh', 
+        'vh', 
+        'ph', 
+        'Ubg', 
+        'Qy',
+    ]
 
+    expected_diags = [
+        'EKE', 
+        'entspec', 
+        'APEflux', 
+        'KEflux', 
+        'APEgenspec', 
+        'APEgen'
+    ]
+
+    expected_attrs = [
+        'L',
+        'W',
+        'dt',
+        'filterfac',
+        'nk',
+        'nl',
+        'ntd',
+        'nx',
+        'ny',
+        'nz',
+        'rek',
+        'taveint',
+        'tavestart',
+        'tc',
+        'tmax',
+        'twrite',
+    ]
+
+    expected_coords = [
+        'time',
+        'lev',
+        'x',
+        'y',
+        'l',
+        'k',
+    ]
+    
     # Initialize QG Model
-    m = pyqg.QGModel(tmax=5*year, twrite=twrite, tavestart=tavestart)
-    m.run()
+    year = 24*60*60*360.
+    m = pyqg.QGModel(tmax=year/2, twrite=10000, tavestart=year/3)
+    tsnapint = year/4
+    
+    # Run with snapshots
+    for snapshot in m.run_with_snapshots(tsnapstart=m.t, tsnapint=tsnapint):
+        ds = m.to_dataset()
+        assert type(ds) == xr.Dataset
 
-    ds = m.to_dataset()
-    
-    assert type(ds) == xr.Dataset
-    
-    expected_vars = ['q', 'u', 'v', 'ph', 'Qy']  
-    for v in expected_vars:
-        assert v in ds
-    
-    expected_attrs = ['L', 'W', 'dt', 'filterfac'] 
-    for a in expected_attrs:
-        assert f"pyqg:{a}" in ds.attrs
+        if snapshot < tsnapint:
 
-    expected_coords = ['x', 'y', 'l', 'k']  
-    for c in expected_coords:
-        assert c in ds.coords
+            for v in expected_vars:
+                assert v in ds
+
+            for a in expected_attrs:
+                assert f"pyqg:{a}" in ds.attrs
+
+            for c in expected_coords:
+                assert c in ds.coords
+
+        if snapshot > tsnapint:
+
+            for v in expected_vars + expected_diags:
+                assert v in ds
+
+            for a in expected_attrs:
+                assert f"pyqg:{a}" in ds.attrs
+
+            for c in expected_coords:
+                assert c in ds.coords

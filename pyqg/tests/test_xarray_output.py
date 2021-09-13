3b7f2db42992c7cc4fc2e65f7d66e0ccce4a253b
hscannell <scannell@ldeo.columbia.edu>
2021-09-13 11:24:26 -0400

    fixture parameters

--- pyqg/tests/test_xarray_output.py
+++ pyqg/tests/test_xarray_output.py
@@ -57,12 +57,10 @@ expected_coords = [
     'k',
 ]
 
-@pytest.fixture
 def QG():
     '''Initialize Two-layer Model'''
     return pyqg.QGModel(tmax=year/2, twrite=10000, tavestart=year/3)
 
-@pytest.fixture
 def Layered():
     '''Initialize Layered quasigeostrophic model'''
     L =  1000.e3     # length scale of box    [m]
@@ -87,55 +85,55 @@ def Layered():
     beta = 1.2130692965249345e-11 # planetary vorticity gradient [m^-1 s^-1]
 
     Ti = Ld/(abs(U1))  # estimate of most unstable e-folding time scale [s]
-    dt = Ti/200.   # time-step [s]
+    dt = Ti/200.       # time-step [s]
     tmax = 300*Ti      # simulation time [s]
 
     return pyqg.LayeredModel(nx=Nx, nz=3, U = [U1,U2,U3],V = [0.,0.,0.],L=L,f=f0,beta=beta,
                              H = [H1,H2,H3], rho=[rho1,rho2,rho3],rek=rek,
                             dt=dt,tmax=tmax, twrite=5000, tavestart=Ti*10)
 
-@pytest.fixture
 def BT():
     '''Initialize Barotropic model'''
     return pyqg.BTModel(L=2.*np.pi, nx=256, beta=0., H=1., rek=0., 
                      rd=None, tmax=40, dt=0.001, taveint=1, ntd=4)
-
     
-@pytest.fixture
 def SQG():
     '''Initialize Surface Quasi-Geostrophic Model'''
     return pyqg.SQGModel(L=2.*np.pi, nx=512, tmax = 26.005, beta = 0., 
                            Nb = 1., H = 1., rek = 0., rd = None, dt = 0.005,
                            taveint=1, twrite=400, ntd=4)
     
-    
-def test_xarray(QG, Layered, BT, SQG):
+@pytest.fixture(params=[QG, Layered, BT, SQG])
+def all_models(request):
+    model = request.param()
+    return model
+
+def test_xarray(all_models):
     '''Run with snapshots and test contents of xarray.dataset'''
-    for i in [QG, Layered, BT, SQG]:
-        m=i
-        tsnapint=year/4
-        for snapshot in m.run_with_snapshots(tsnapstart=m.t, tsnapint=tsnapint):
-            ds = m.to_dataset()
-            assert type(ds) == xr.Dataset
+    m=all_models
+    tsnapint=year/4
+    for snapshot in m.run_with_snapshots(tsnapstart=m.t, tsnapint=tsnapint):
+        ds = m.to_dataset()
+        assert type(ds) == xr.Dataset
 
-            if snapshot < tsnapint:
+        if snapshot < tsnapint:
 
-                for v in expected_vars:
-                    assert v in ds
+            for v in expected_vars:
+                assert v in ds
 
-                for a in expected_attrs:
-                    assert f"pyqg:{a}" in ds.attrs
+            for a in expected_attrs:
+                assert f"pyqg:{a}" in ds.attrs
 
-                for c in expected_coords:
-                    assert c in ds.coords
+            for c in expected_coords:
+                assert c in ds.coords
 
-            if snapshot > tsnapint:
+        if snapshot > tsnapint:
 
-                for v in expected_vars + expected_diags:
-                    assert v in ds
+            for v in expected_vars + expected_diags:
+                assert v in ds
 
-                for a in expected_attrs:
-                    assert f"pyqg:{a}" in ds.attrs
+            for a in expected_attrs:
+                assert f"pyqg:{a}" in ds.attrs
 
-                for c in expected_coords:
-                    assert c in ds.coords
+            for c in expected_coords:
+                assert c in ds.coords

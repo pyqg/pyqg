8458a67b179dd24d0536b66ec890570be04aad27
Andrew Ross <andrewslavinross@gmail.com>
2022-03-05 13:34:16 -0500

    Attempt to fix docs / python <3.8 compatibility

--- pyqg/__init__.py
+++ pyqg/__init__.py
@@ -5,7 +5,11 @@ from .bt_model import BTModel
 from .sqg_model import SQGModel
 from .layered_model import LayeredModel
 from .particles import LagrangianParticleArray2D, GriddedLagrangianParticleArray2D
-from importlib.metadata import version, PackageNotFoundError
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 try:
     __version__ = version("pyqg")

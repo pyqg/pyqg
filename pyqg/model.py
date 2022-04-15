055e125178f1abf1f7ea0d1d755531be371e1dca
Andrew Ross <andrewslavinross@gmail.com>
2022-04-15 12:54:59 -0600

    Remove accidentally recommited paramspec from main model.py

--- pyqg/model.py
+++ pyqg/model.py
@@ -639,31 +639,6 @@ class Model(PseudoSpectralKernel):
             dims=('lev',)
         )
 
-        def parameterization_spectrum(m):
-            spectrum = np.zeros_like(m.wv2)
-
-            if m.uv_parameterization is not None:
-                ik = np.asarray(m._ik).reshape((1, -1)).repeat(m.wv2.shape[0], axis=0)
-                il = np.asarray(m._il).reshape((-1, 1)).repeat(m.wv2.shape[-1], axis=-1)
-                dqh1 = (-il * m.duh[0] + ik * m.dvh[0])
-                dqh2 = (-il * m.duh[1] + ik * m.dvh[1])
-                if m.q_parameterization is not None:
-                    dqh1 += m.dqh[0]
-                    dqh2 += m.dqh[1]
-                spectrum += m._calc_parameterization_spectrum(dqh1, dqh2)
-
-            elif m.q_parameterization is not None:
-                spectrum += m._calc_parameterization_spectrum(*m.dqh)
-
-            return spectrum
-
-        self.add_diagnostic('paramspec',
-            description='Spectral contribution of subgrid parameterization (if present)',
-            function=parameterization_spectrum,
-            units='',
-            dims=('l','k')
-        )
-
         def dissipation_spectrum(m):
             spectrum = np.zeros_like(m.qh)
             ones = np.ones_like(m.filtr)

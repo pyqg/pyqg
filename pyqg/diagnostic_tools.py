Modifying calc_ispec and adding a test for it. Changing kmin in calc_ispec() to include all the way to k = 0, Fixing a bug which does not count some wavenumbers due to numerical issues: (from fkr = (model.wv>=kr[i]) & (model.wv<kr[i]+dkr) to fkr = (model.wv>=kr[i]) & (model.wv<kr[i+1])) The above ensures that Parseval's theorem is satisfied exactly. To see how, we are testing this with test_calc_ispec_sum() in test_diagnostic_tools.py.

pyqg/diagnostic_tools.py
@@ -93,7 +93,7 @@ def calc_ispec(model, _var_dens, averaging = True, truncate=True, nd_wavenumber=
     else:
         kmax = np.sqrt(ll_max**2 + kk_max**2)
     
-    kmin = np.minimum(model.dk, model.dl)
+    kmin = 0
 
     dkr = np.sqrt(model.dk**2 + model.dl**2) * nfactor
 
@@ -106,13 +106,13 @@ def calc_ispec(model, _var_dens, averaging = True, truncate=True, nd_wavenumber=
         if i == kr.size-1:
             fkr = (model.wv>=kr[i]) & (model.wv<=kr[i]+dkr)
         else:
-            fkr = (model.wv>=kr[i]) & (model.wv<kr[i]+dkr)
+            fkr = (model.wv>=kr[i]) & (model.wv<kr[i+1])
         if averaging:
             phr[i] = var_dens[fkr].mean() * (kr[i]+dkr/2) * pi / (model.dk * model.dl)
         else:
             phr[i] = var_dens[fkr].sum() / dkr
 
-        #phr[i] *= 2 # include full circle
+        phr[i] *= 2 # include full circle
     
     # convert left border of the bin to center
     kr = kr + dkr/2
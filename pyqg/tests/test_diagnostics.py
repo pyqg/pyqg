c85d2e4fdce0c75d600681a92a16d9373b887de8
Karl Otness <karl.otness@nyu.edu>
2023-02-28 20:12:23 -0500

    Remove some unused imports in tests.

--- pyqg/tests/test_diagnostics.py
+++ pyqg/tests/test_diagnostics.py
@@ -1,11 +1,9 @@
 from future.utils import iteritems
 import pytest
-import unittest
 import numpy as np
 import pyqg
 import pickle
 import os
-from pyqg import diagnostic_tools as diag
 
 def test_describe_diagnostics():
     """ Test whether describe_diagnostics runs without error """
